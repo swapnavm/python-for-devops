35c1f841410455e0dffb27df37e9974445cea85b
swapnavm <145467836+swapnavm@users.noreply.github.com>
2023-11-08 11:18:01 -0500

    Update 01-string-substring.py

--- Day-02/examples/01-string-substring.py
+++ Day-02/examples/01-string-substring.py
@@ -2,3 +2,7 @@ text = "Python is awesome"
 substring = "is"
 if substring in text:
     print(substring, "found in the text")
+institute = "Michigan Institute Of Technology"
+substring = "of"
+if substring in institute:
+    print(substring, "found in the text")
